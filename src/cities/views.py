bfb69c64d2b870c69b4f3606c1273719d7d21c18
DVorobiev1968 <vdv@rdturbo.ru>
2021-09-30 11:28:24 +0500

    Разработаны функции создания, редактирования, удаления городов.

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from cities.forms import HtmlForm, CityForm
+from cities.models import City
+
+__all__ = (
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView',
+    'CityDeleteView'
+)
+
+
+def home(request, pk=None):
+    if request.method == 'POST':
+        # вывод в консоль, т.к. файл отображения уже задан
+        form = CityForm(request.POST)
+        # проверим все ли типы данных заявленных в форме корректны, в том числе уникальность
+        # в случае успеха выведем данные в консоль
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    # pk будет передаваться через адресную строку
+    if pk:
+        # используем метод filter для того чтобы не было ошибки если зададим несуществующий pk
+        # city=City.objects.filter(id=pk).first()
+        # этот метод уже возвратит ошибку и его нужно использовать с конструкцией try... exception, чтобы такую ошибку ловить
+        # city = City.objects.get(id=pk)
+        # либо получаем ошибку о несуществующем объекте через функцию фреймворка django
+        city = get_object_or_404(City, id=pk)
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
+    form = CityForm()
+    qs = City.objects.all()
+    # lst = Paginator(qs, 2)
+    # page_number=request.GET.get('page')
+    # page_obj=lst.get_page(page_number)
+    # context={'page_list':page_obj,'form':form}
+    context = {'object_list': qs, 'form': form}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    # для плдтверждения удаления перенаправляем на специальную для этого страницу
+    # template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+    # либо делаем безусловное удаление
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
--- src/travel/urls.py
+++ src/travel/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from travel.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',home),
+    path('cities/',include(('cities.urls','cities'))),
+    path('',home,name='home'),
     path('about/', about),
 ]
