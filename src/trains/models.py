7ce3ead236a6b7b0578a078c391103a676c395b2
DVorobiev1968 <vdv@rdturbo.ru>
2021-10-01 11:48:15 +0500

    Урок 45-46 Улучшение внешнего вида админки.

--- src/trains/models.py
+++ src/trains/models.py
@@ -5,6 +5,7 @@ from cities.models import City
 
 
 class Train(models.Model):
+    objects = None
     name = models.CharField(max_length=50, unique=True,
                             verbose_name='Номер поезда')
     travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути (час)')
@@ -14,18 +15,18 @@ class Train(models.Model):
     # on_delete=models.SET_NULL при удалении записи, значение устанавливается в NULL,
     # обязательные параметры:
     #   null=True, blank=True
-    from_sity = models.ForeignKey(City, on_delete=models.CASCADE,
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE,
                                   # null=True, blank=True,
-                                  related_name='from_sity_set',
+                                  related_name='from_city_set',
                                   verbose_name='Из какого города'
                                   )
     to_city = models.ForeignKey('cities.City', on_delete=models.CASCADE,
-                                related_name='to_sity_set',
+                                related_name='to_city_set',
                                 verbose_name='В какой город'
                                 )
 
     def __str__(self):
-        return f'Поезд номер:{self.name} из города {self.from_sity}'
+        return f'Поезд номер:{self.name} из города {self.from_city}'
 
     class Meta:
         verbose_name = 'Поезд'
@@ -33,9 +34,9 @@ class Train(models.Model):
         ordering = ['travel_time']
 
     def clean(self):
-        if self.from_sity == self.to_city:
+        if self.from_city == self.to_city:
             raise ValidationError('Изменить город прибытия')
-        qs = Train.objects.filter(from_sity=self.from_sity,
+        qs = Train.objects.filter(from_city=self.from_city,
                                   to_city=self.to_city,
                                   travel_time=self.travel_time).exclude(pk=self.pk)
         # Train == self.__class__
