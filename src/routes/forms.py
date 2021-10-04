cfdf97bbc5b7ab2f2da96aae52ab7c492f898695
DVorobiev1968 <vdv@rdturbo.ru>
2021-10-04 09:54:47 +0500

    Урок 52. доработка функции множественного выбора в routes/forms.py, routes/base.html. В проекте используется Bootstrap 5 без поддержки JQuery, поэтому есть расхождения. Проработать вопрос множественного выбора самостоятельно.

--- src/routes/forms.py
+++ src/routes/forms.py
@@ -12,11 +12,11 @@ class RouteForm(forms.Form):
         }))
     from_city = forms.ModelChoiceField(
         label='Откуда', queryset=City.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control'}))
+        widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
     to_city = forms.ModelChoiceField(
         label='Куда', queryset=City.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control'}))
+        widget=forms.Select(attrs={'class': 'form-control js-example-basic-single'}))
     cities = forms.ModelMultipleChoiceField(
         label='Через города', queryset=City.objects.all(),
         required=False,
-        widget=forms.SelectMultiple(attrs={'class': 'form-control'}))
+        widget=forms.SelectMultiple(attrs={'class': 'form-control js-example-basic-multiple'}))
