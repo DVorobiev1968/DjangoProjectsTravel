9681905aad270d779a58ef41d0a5211a6bcace0f
DVorobiev1968 <vdv@rdturbo.ru>
2021-10-06 14:30:32 +0500

    Урок 68. Сохранение маршрута

--- src/travel/settings.py
+++ src/travel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y0gpgxf#yrrw_kz+vgg$he4o3$s8n$d&uttkw!cwj+!pqq$!@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- src/travel/urls.py
+++ src/travel/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from routes.views import home, find_routes,add_route
+from routes.views import home, find_routes,add_route,save_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +24,6 @@ urlpatterns = [
     path('trains/', include(('trains.urls', 'trains'))),
     path('find_routes/', find_routes, name='find_routes'),
     path('add_route/', add_route, name='add_route'),
+    path('save_route/', save_route, name='save_route'),
     path('', home, name='home'),
 ]
