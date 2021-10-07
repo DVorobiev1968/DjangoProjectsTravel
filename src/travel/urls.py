72a9b20656bc88e342f42da62880f148df0c6d9f
DVorobiev1968 <vdv@rdturbo.ru>
2021-10-07 10:31:27 +0500

    Урок 72. Система аутентификации, доступ к оперделенным разделам разрешенным пользователям.

--- src/travel/urls.py
+++ src/travel/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from routes.views import home, find_routes,add_route,save_route,RouteListView,RouteDetailView
+from routes.views import \
+    home, find_routes, add_route, save_route, \
+    RouteListView, RouteDetailView, RouteDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +29,6 @@ urlpatterns = [
     path('add_route/', add_route, name='add_route'),
     path('save_route/', save_route, name='save_route'),
     path('list/', RouteListView.as_view(), name='list'),
-     path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
+    path('delete/<int:pk>/', RouteDeleteView.as_view(), name='delete'),
 ]
