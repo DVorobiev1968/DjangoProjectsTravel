324e40b5c87583598083b71f420db811955982c2
DVorobiev1968 <vdv@rdturbo.ru>
2021-10-08 13:53:08 +0500

    Урок 88. Тестирование возникновения ошибки при создании дубля города. Тестирование возникновения ошибки при создании дубля поезда.

--- src/routes/tests.py
+++ src/routes/tests.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from cities.models import City
@@ -13,15 +14,46 @@ class AllTestsCase(TestCase):
         self.city_C = City.objects.create(name='C')
         self.city_D = City.objects.create(name='D')
         self.city_E = City.objects.create(name='E')
-        lst=[
-           Train(name='t1',from_city=self.city_A,to_city=self.city_B,travel_time=9),
-           Train(name='t2',from_city=self.city_B,to_city=self.city_D,travel_time=8),
-           Train(name='t3',from_city=self.city_A,to_city=self.city_C,travel_time=7),
-           Train(name='t4',from_city=self.city_C,to_city=self.city_B,travel_time=6),
-           Train(name='t5',from_city=self.city_B,to_city=self.city_E,travel_time=3),
-           Train(name='t6',from_city=self.city_B,to_city=self.city_A,travel_time=11),
-           Train(name='t7',from_city=self.city_A,to_city=self.city_C,travel_time=10),
-           Train(name='t8',from_city=self.city_E,to_city=self.city_D,travel_time=5),
-           Train(name='t9',from_city=self.city_D,to_city=self.city_E,travel_time=4),
+        lst = [
+            Train(name='t1', from_city=self.city_A, to_city=self.city_B, travel_time=9),
+            Train(name='t2', from_city=self.city_B, to_city=self.city_D, travel_time=8),
+            Train(name='t3', from_city=self.city_A, to_city=self.city_C, travel_time=7),
+            Train(name='t4', from_city=self.city_C, to_city=self.city_B, travel_time=6),
+            Train(name='t5', from_city=self.city_B, to_city=self.city_E, travel_time=3),
+            Train(name='t6', from_city=self.city_B, to_city=self.city_A, travel_time=11),
+            Train(name='t7', from_city=self.city_A, to_city=self.city_C, travel_time=10),
+            Train(name='t8', from_city=self.city_E, to_city=self.city_D, travel_time=5),
+            Train(name='t9', from_city=self.city_D, to_city=self.city_E, travel_time=4),
         ]
         Train.objects.bulk_create(lst)
+
+    def test_model_city_duplicate(self):
+        """
+        Тестирование возникновения ошибки при создании дубля города
+        :return:
+        """
+        city = City(name='A')
+        with self.assertRaises(ValidationError):
+            city.full_clean()
+
+    def test_model_train_duplicate(self):
+        """
+        Тестирование возникновения ошибки при создании дубля поезда
+        :return:
+        """
+        train = Train(name='t1', from_city=self.city_A, to_city=self.city_B, travel_time=129)
+        with self.assertRaises(ValidationError):
+            train.full_clean()
+
+    def test_model_train_duplicate_1(self):
+        """
+        Тестирование возникновения ошибки при создании дубля поезда
+        :return:
+        """
+        train = Train(name='t1234', from_city=self.city_A, to_city=self.city_B, travel_time=9)
+        with self.assertRaises(ValidationError):
+            train.full_clean()
+        try:
+            train.full_clean()
+        except  ValidationError as e:
+            self.assertEqual({'__all__':['Измените время в пути']},e.message_dict)
